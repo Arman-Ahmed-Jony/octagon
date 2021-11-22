2fb8e90b0e665d7ff08ea301ba359b751ee68497
arman-ahmed-jony <armanahmedjony786@gmail.com>
2021-11-23 00:31:33 +0600

    added login api

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,8 @@ const bcrypt = require("bcrypt");
 const express = require("express");
 const _ = require("lodash");
 const UserService = require("../service/UserService");
-const authenticationUtil =  require('../util/authenticaitonUtil')
+const authenticationUtil = require("../util/authenticaitonUtil");
+const responseBeautifier = require("../middleware/responseBeautifier");
 
 const router = express.Router();
 
@@ -18,23 +19,51 @@ router.post("/register", async (req, res) => {
     const salt = await bcrypt.genSalt(); // by default salt is 10
     req.body.password = await bcrypt.hash(req.body.password, salt);
 
-    const newUser = await UserService.create(
-      {
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.body.email,
-        password: req.body.password
-      }
-    )
+    const newUser = await UserService.create({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      password: req.body.password,
+    });
 
     const token = authenticationUtil.generateJwtToken(newUser, "blah");
     res
       .status(201)
       .header("x-auth-token", token)
-      .json(_.pick(newUser, ["id", "firstName", "email"]));
+      .json(_.pick(newUser, ["firstName", "lastName", "email"]));
   } catch (error) {
     res.status(400).json({ message: error });
   }
 });
 
+router.post(
+  "/login",
+  async (req, res) => {
+    try {
+      const user = await UserService.findByEmail(req.body.email);
+      console.log("test", user);
+      if (!user) {
+        res.status(401).json({ message: "Invalid email or password" });
+      }
+      const validPassword = await bcrypt.compare(
+        req.body.password,
+        user.password
+      );
+      if (!validPassword) {
+        res.status(401).json({ message: "Invalid email or password" });
+      }
+      const token = authenticationUtil.generateJwtToken(user, "blah");
+      res
+        .status(200)
+        .header("x-auth-token", token)
+        .json(_.pick(user, ["firstName", "lastName", "email"]));
+    } catch (error) {
+      req.body = error;
+      req.responseStatus = 500;
+    } finally {
+      next();
+    }
+  },
+  responseBeautifier
+);
 module.exports = router;
